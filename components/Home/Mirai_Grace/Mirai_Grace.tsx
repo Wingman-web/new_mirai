f1facbcf7a5f58be97b4d99cb369e91381fadf66
rahul_singhsss <rs913050@gmail.com>
2025-12-31 10:36:33 +0530

    feat(Mirai_Grace): replace heading with highlight video

--- components/Home/Mirai_Grace/Mirai_Grace.tsx
+++ components/Home/Mirai_Grace/Mirai_Grace.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Mirai_Grace = () => {
   return (
-    <div>
-      <h1 className='bg-black'>history</h1>
+    <div className="bg-white text-black w-full">
+      <video
+        src="/highlight.mp4"
+        className="block w-full h-screen object-cover"
+        autoPlay
+        loop
+        muted
+        playsInline
+        aria-label="Highlight video"
+      />
     </div>
   )
 }
