7baadec12b3432f6082368bd086a4ecb31fc80a7
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-07 15:25:01 +0530

    Add files via upload

---
+++ app/live-the-sixth-element/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PavaniMiraiBlog from '@/components/live-the-sixth-element/page'
+
+const page = () => {
+  return (
+    <PavaniMiraiBlog />
+  )
+}
+
+export default page
