50199929a87333006bb1f409fcdb6b63db00264c
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-08 11:04:50 +0530

    Update Page.tsx

--- components/Amenities/Hero/Page.tsx
+++ components/Amenities/Hero/Page.tsx
@@ -13,8 +13,19 @@ const AmenitiesHero = () => {
         priority
         unoptimized
       />
-
-
+      
+      {/* Logo with Lines */}
+      <div className="absolute top-8 left-0 right-0 z-20 flex items-center px-8">
+        <div className="flex-1 h-[1px] bg-white"></div>
+        <div className="px-8">
+          <img
+            src="https://azure-baboon-302476.hostingersite.com/mirai_latest/media/logo.png"
+            alt="Pavani Mirai Logo"
+            className="h-16 w-auto"
+          />
+        </div>
+        <div className="flex-1 h-[1px] bg-white"></div>
+      </div>
     </section>
   )
 }
