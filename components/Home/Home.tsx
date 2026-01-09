c06d7b974db7f10ef3432a49e0fb4fa21596d738
Rahul Singh <62008048+rahulsinghsss@users.noreply.github.com>
2026-01-09 15:22:22 +0530

    Update Home.tsx

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,7 @@
+'use client'; // Add this at the top if using App Router
+
 import React from 'react'
+import VideoPreloader from './VideoPreloader' // Import the preloader
 import Hero from './Hero/Hero'
 import { RevealZoom } from './Gateway/Gateway' 
 import Mirai_Grace from './Mirai_Grace/Mirai_Grace'
@@ -13,50 +16,54 @@ import SixthElement from './Sixth_Element/Sixth_element'
 
 const Home = () => {
   return (
-    <div className="relative bg-black w-full overflow-x-hidden">
-      <div className="relative z-10 bg-black">
-        <Hero />
-        
-        {/* Spacer for Hero */}
-        <div className="h-screen" aria-hidden="true" />
-        
-        {/* SixthElement - Using z-index 10 */}
-        <div className="relative" style={{ zIndex: 10 }}>
-          <SixthElement />
+    <>
+      {/* Video Preloader - highest z-index */}
+      <VideoPreloader />
+      
+      <div className="relative bg-black w-full overflow-x-hidden">
+        <div className="relative z-10 bg-black">
+          <Hero />
+          
+          {/* Spacer for Hero */}
+          <div className="h-screen" aria-hidden="true" />
+          
+          {/* SixthElement - Using z-index 10 */}
+          <div className="relative" style={{ zIndex: 10 }}>
+            <SixthElement />
+          </div>
+          
+          {/* RevealZoom - Using z-index 11 (next in line) */}
+          <section 
+            aria-label="Reveal zoom" 
+            className="relative bg-black"
+            style={{ zIndex: 11, isolation: 'isolate' }}
+          >
+            <RevealZoom />
+          </section>
+          
+          {/* Following sections continue the ladder */}
+          <section 
+            aria-label="Scroll video" 
+            className="relative bg-black"
+            style={{ zIndex: 12 }}
+          >
+            <Mirai_Grace />
+          </section>
+          
+          <div style={{ position: 'relative', zIndex: 13 }}>
+            <MiraiPodsIntro />
+            <MiraiPodsSlider />
+            <ClubhouseIntro />
+            <MiraiClubhouse />
+            <InteractiveMap />
+          </div>
         </div>
-        
-        {/* RevealZoom - Using z-index 11 (next in line) */}
-        <section 
-          aria-label="Reveal zoom" 
-          className="relative bg-black"
-          style={{ zIndex: 11, isolation: 'isolate' }}
-        >
-          <RevealZoom />
-        </section>
-        
-        {/* Following sections continue the ladder */}
-        <section 
-          aria-label="Scroll video" 
-          className="relative bg-black"
-          style={{ zIndex: 12 }}
-        >
-          <Mirai_Grace />
-        </section>
-        
-        <div style={{ position: 'relative', zIndex: 13 }}>
-          <MiraiPodsIntro />
-          <MiraiPodsSlider />
-          <ClubhouseIntro />
-          <MiraiClubhouse />
-          <InteractiveMap />
-        </div>
+        {/* Fixed UI layers remain at the bottom/top of the stack */}
+        <ContactForm />
+        <div className="relative h-screen" style={{ zIndex: 0 }} />
+        <Footer />
       </div>
-
-      {/* Fixed UI layers remain at the bottom/top of the stack */}
-      <ContactForm />
-      <div className="relative h-screen" style={{ zIndex: 0 }} />
-      <Footer />
-    </div>
+    </>
   )
 }
 
